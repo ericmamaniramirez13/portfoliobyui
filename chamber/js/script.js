ecda350bf7dde620486e8ebf64947dd9c8225161
ericmamaniramirez13 <ericmamaniramirez13@gmail.com>
2022-02-03 23:42:54 -0700

    chamber changes

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,4 +1,18 @@
-let currentdate = document.lastModified
-let time = document.querySelector("#modified")
-time.textContent += currentdate;
+const ham = document.querySelector(".ham");
+const navigation = document.querySelector(".navigation");
+ham.addEventListener("click", () => {navigation.classList.toggle("responsive")});
+
+let today_date = new Date();
+console.log(today_date);
+
+weekday = Intl.DateTimeFormat('en-US', {weekday: "long"}).format(today_date);
+day = today_date.getDate();
+month = Intl.DateTimeFormat('en-US', {month: "long"}).format(today_date);
+year = today_date.getFullYear();
+const date = document.querySelector(".date");
+date.textContent = `${weekday}, ${day} ${month} ${year}`
+
+let lastTime = document.lastModified;
+let time = document.querySelector("#modified");
+time.textContent += `Last Updated: ${lastTime}`;
 time.style.color = "white";

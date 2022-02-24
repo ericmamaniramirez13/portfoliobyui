08e0199a81444e5b0997fdb0cb9516928013756e
ericmamaniramirez13 <ericmamaniramirez13@gmail.com>
2022-02-24 02:25:35 -0700

    discover page

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -34,3 +34,5 @@ var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.42
 
 var windChill= Math.round(windChill);
 document.getElementById("wind").innerHTML= windChill;
+
+//Discover Script

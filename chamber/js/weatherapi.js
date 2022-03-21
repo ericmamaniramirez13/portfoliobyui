d1b9281ea801a73f148a3dca1260208d01623f17
ericmamaniramirez13 <ericmamaniramirez13@gmail.com>
2022-03-20 18:56:13 -0600

    update on directory

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -8,12 +8,12 @@ fetch(apiURL).then((response) => response.json()).then((jsObject) => {
   const weatherDesc = jsObject.weather[0].description;
   const windSpeed = jsObject.wind.speed;
   const windChill = Math.round(35.74 + 0.6215 * temp - 35.75 * (Math.pow(windSpeed, 0.16)) + 0.4275 * temp * (Math.pow(windSpeed, 0.16)));
-
+  //35.74 + 0.6215×T - 35.75×V0.16 + 0.4275×T×V0.16
   document.querySelector('#current-temp').textContent = temp+"°F";
   document.querySelector('#weathericon').setAttribute('src', iconsrc);
   document.querySelector('#weathericon').setAttribute('alt', weatherDesc);
   document.querySelector('#condition').textContent = weatherDesc;
-  document.querySelector('#wind-speed').textContent = windSpeed;
+  document.querySelector('#wind-speed').textContent = windSpeed + " Mph";
   document.querySelector('#wind-chill').textContent = windChill;
 });
 

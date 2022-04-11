0d02a1167f7fd27fef209ffa89f9ab25d58b60b5
Eric Mamani <75715319+ericmamaniramirez13@users.noreply.github.com>
2022-04-11 07:32:47 -0600

    Delete final directory

--- final/js/toggle.js
+++
@@ -1,7 +0,0 @@
-const toggleMenu = () => {
-    document.querySelector('#nav').classList.toggle('open');
-}
-
-let hamBtn = document.querySelector('#hamBtn');
-
-hamBtn.addEventListener('click', toggleMenu);
